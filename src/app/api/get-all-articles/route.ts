d5c3b5e9eacb1038b9a2b02943d274d74540e2ee
Rohan Gope <rg2512@it.jgec.ac.in>
2024-07-21 18:56:54 +0530

    refactor: Update lazy loading configuration for images and fix project description alignment

--- src/app/api/get-all-articles/route.ts
+++ src/app/api/get-all-articles/route.ts
@@ -21,7 +21,7 @@ export async function GET() {
         return NextResponse.json({
             success: true,
             message: 'Articles fetched successfully',
-            data: articles
+            articles: articles
         }, { status: 200 })
     } catch (error: any) {
         return NextResponse.json({
